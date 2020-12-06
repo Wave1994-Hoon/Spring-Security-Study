c5765edd35c43a13c426e9cebfa50d34cb7fcbda
kwanghoon.kim <kk03187693@gmail.com>
2020-12-06 22:51:36 +0900

    add 사용자 정의 보안 기능

---
+++ src/main/java/io/security/basicsecurity/SecurityConfig.java
@@ -0,0 +1,18 @@
+package io.security.basicsecurity;
+
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+            .authorizeRequests()
+            .anyRequest().authenticated();    // 어떤 요청에도 인증을 받음
+
+        http
+            .formLogin();
+    }
+}
